e21d89deb9db1c9c0450bcf59ca30ef38f1f55e5
dnfm257 <73047407+dnfm257@users.noreply.github.com>
2023-11-09 15:23:01 +0900

    final

--- work_python/test/recv_ocr.py
+++ work_python/test/recv_ocr.py
@@ -30,6 +30,12 @@ def preprocess_image(img):
     
     return dst_img
 
+# 관심영역 설정
+def set_Roi(frame, roi_x_start, roi_y_start, roi_width, roi_height):
+    ROI = frame[roi_y_start: roi_y_start+roi_height, roi_x_start: roi_x_start+roi_width]
+    
+    return ROI
+
 # 많이 검출된 단어 뽑기
 def detect_word(detected_words): 
     most_detected_words = max(detected_words, key=detected_words.get)
@@ -46,17 +52,75 @@ def most_area_word(area):
     
     return most_area_word
     
+# 검출된 단어를 조건에 맞게 뽑기
+def search(frame, result, area, roi_x_start, roi_y_start):
+    global x, y, w, h
+    word = None
+    
+    for line in result:
+        points = np.array([line[0][0], line[0][1], line[0][2], line[0][3]], dtype=np.int0)
+        
+        if line[1] is None:
+            continue
+        
+        word, confidence = line[1]
+    
+        # 인식단어 길이가 2 이상 4 이하인 단어만 통과
+        if len(word) < 2 or len(word) > 4:
+            return None
+        
+        # 텍스트 위치에서 사각형 찾기
+        x, y, w, h = cv2.boundingRect(points)
+        
+        # 신뢰도가 일정 이하일 때 재탐색
+        if confidence < 0.6:
+            if not is_correct(frame, word, roi_x_start, roi_y_start, inflate=10):
+                return None
+        
+        area[word] = w * h
+    
+    # 해당 장면에서 넓이가 제일 넓은 단어만 추출
+    if area:
+        frame_word = most_area_word(area)
+        return frame_word
+    
+    # 예외 : 길이가 짧은 단어만 검출됐을 경우
+    return None
+
+# 텍스트 박스 만들기
+def draw_text_box(frame, frame_word, roi_x_start, roi_y_start):
+    real_x, real_y = (int)(x + roi_x_start), (int)(y + roi_y_start)
+    cv2.rectangle(frame, (real_x, real_y), (real_x+w, real_y+h), (0, 255, 0), 2)
+    cv2.putText(frame, frame_word, (real_x, real_y - 8), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+# 신뢰도 낮은 단어에 대해 한번 더 체크
+def is_correct(frame, word, roi_x_start, roi_y_start, inflate):
+    Rx, Ry = (int)(x + roi_x_start), (int)(y + roi_y_start)
+    subROI = set_Roi(frame, Rx-inflate, Ry-inflate, (int)(w+inflate*2), (int)(h+inflate*2))
+    
+    pre_subimg = preprocess_image(subROI)
+    result_img = cv2.bilateralFilter(pre_subimg, -1, 10, 5)
+    
+    sub_result = ocr.ocr(result_img, cls=True)
+    cv2.imshow('correct', result_img)
+    
+    # 단어 인식이 안됐거나 기존 단어와 다르게 인식된 경우
+    if (sub_result[0] is None) or (sub_result[0][0][1][0] != word):
+        return False
+        
+    return True
+
 # 서버로 단어 전송할 코드
 def send_string(most_detfloorWords, most_detlocationWords):
     data = (f'차량의 위치는 {most_detfloorWords}층 {most_detlocationWords}구역입니다.')
     
-    """
+    
     #데이터 전송 = TCP
     sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock2.connect(('10.10.141.22', 6001)) # 접속할 서버의 ip주소와 포트번호를 입력.
     sock2.send(data.encode())
-    """
     
+    """
     # 모바일 전송 코드
     account_sid = 'AC830601052a526b757f23cac741e8becb'
     auth_token = ''
@@ -65,10 +129,10 @@ def send_string(most_detfloorWords, most_detlocationWords):
         from_='+12563685788',
         body=data.encode(),
         to='+821031198106'
-    )
+    )"""
     print("message Send")
     
-    #sock2.close()
+    sock2.close()
 
 # PaddleOCR 호출 및 옵션 설정(cuda 가속 여부, 언어, inference모델)
 ocr = PaddleOCR(
@@ -87,7 +151,7 @@ buf_size = 1000000 # 600*480*3 = 864,000
 floorWords = defaultdict(int)
 locationWords = defaultdict(int)
 
-# 이미지 크기(영상 비율 유지, 웹캠(5:4) 영상(16:9))
+# 이미지 크기(영상 비율 유지바람, 웹캠(5:4) 영상(16:9))
 width = 640
 height = 512
 
@@ -114,7 +178,7 @@ try:
         frame = cv2.resize(img, (width, height), cv2.INTER_CUBIC) # 해상도 조절
             
         # ROI(관심영역) 설정
-        ROI = frame[roi_y_start: roi_y_start+roi_height, roi_x_start: roi_x_start+roi_width]
+        ROI = set_Roi(frame, roi_x_start, roi_y_start, roi_width, roi_height)
         
         # 전처리
         pre_img = preprocess_image(ROI)
@@ -125,41 +189,26 @@ try:
             
         # 텍스트 인식한 게 있다면
         if result[0] is not None:
-                
+            
             area = defaultdict(int) # 넓이 저장
-                
+            
             # 프레임 한장에 나오는 단어 개수만큼
-            for line in result[0]:
-                points = np.array([line[0][0], line[0][1], line[0][2], line[0][3]], dtype=np.int0)
-                word, confidence = line[1]
-                    
-                # 인식단어 길이가 2 이상 4 이하인 단어만 통과
-                if len(word) < 2 or len(word) > 4:
-                    continue
-                    
-                # 텍스트 위치에서 가장 작은 사각형 찾기
-                x, y, w, h = cv2.boundingRect(points)
-                    
-                # 텍스트 넓이 저장
-                area[word] = w * h
-                    
-            # 해당 장면에서 넓이가 제일 넓은 단어만 추출
-            if area:
-                frame_word = most_area_word(area)
-            else: # 예외 : 길이가 짧은 단어만 검출됐을 경우
+            try:
+                area_sample, word = search(frame, result[0], area, roi_x_start, roi_y_start)
+            except Exception as e:
+                print(f"Exception: {str(e)}")
+                
+            if word is None:
                 continue
                     
             # 단어(key)에 대한 카운트(value)
-            if frame_word[0] == 'B' or frame_word[len(frame_word) - 1] == 'F':
-                floorWords[frame_word] += 1 # 층수
+            if word[0] == 'B' or word[len(word) - 1] == 'F':
+                floorWords[word] += 1 # 층수
             else:
-                locationWords[frame_word] += 1 # 구역
+                locationWords[word] += 1 # 구역
                         
             # 텍스트 box만들기
-            str = frame_word #+ " {:.2f}".format(confidence)
-            real_x, real_y = (int)(x + roi_x_start), (int)(y + roi_y_start)
-            cv2.rectangle(frame, (real_x, real_y), (real_x+w, real_y+h), (0, 255, 0), 2)
-            cv2.putText(frame, str, (real_x, real_y - 8), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+            draw_text_box(frame, word, roi_x_start, roi_y_start)
   
         cv2.imshow('video', frame)
             
@@ -180,7 +229,7 @@ try:
     # 클라이언트에게 텍스트 전송
     send_string(most_detfloorWords, most_detlocationWords)
     
- 
+
 # 오류 catch 
 except Exception as e:
     print(f"Exception: {str(e)}")
--- work_python/test3/video_play.py
+++ work_python/test3/video_play.py
@@ -28,12 +28,18 @@ def preprocess_image(img):
     
     return dst_img
 
+# 관심영역 설정
+def set_Roi(frame, roi_x_start, roi_y_start, roi_width, roi_height):
+    ROI = frame[roi_y_start: roi_y_start+roi_height, roi_x_start: roi_x_start+roi_width]
+    
+    return ROI
+
 # 많이 검출된 단어 찾기
 def detect_word(detected_words): 
     most_detected_words = max(detected_words, key=detected_words.get)
     
-    # 한번만 검출되면 무시
-    if detected_words[most_detected_words] == 1:
+    # 세번 이하로 검출되면 무시(쓰레기값)
+    if detected_words[most_detected_words] <= 3:
         most_detected_words = "None"
     
     return most_detected_words
@@ -43,6 +49,64 @@ def most_area_word(area):
     most_area_word = max(area, key=area.get)
     
     return most_area_word
+
+# 검출된 단어를 조건에 맞게 뽑기
+def search(frame, result, area, roi_x_start, roi_y_start):
+    global x, y, w, h
+    word = None
+    
+    for line in result:
+        points = np.array([line[0][0], line[0][1], line[0][2], line[0][3]], dtype=np.int0)
+        
+        if line[1] is None:
+            continue
+        
+        word, confidence = line[1]
+        
+        # 인식단어 길이가 2 이상 4 이하인 단어만 통과
+        if len(word) < 2 or len(word) > 4:
+            return None
+        
+        # 텍스트 위치에서 사각형 찾기
+        x, y, w, h = cv2.boundingRect(points)
+        
+        # 신뢰도가 일정 이하일 때 재인식
+        if confidence < 0.6:
+            if not is_correct(frame, word, roi_x_start, roi_y_start, inflate=10):
+                return None
+        
+        area[word] = w * h
+    
+    # 해당 장면에서 넓이가 제일 넓은 단어만 추출
+    if area:
+        frame_word = most_area_word(area)
+        return frame_word
+    
+    # 예외 : 길이가 짧은 단어만 검출됐을 경우
+    return None
+
+# 텍스트 박스 만들기
+def draw_text_box(frame, frame_word, roi_x_start, roi_y_start):
+    real_x, real_y = (int)(x + roi_x_start), (int)(y + roi_y_start)
+    cv2.rectangle(frame, (real_x, real_y), (real_x+w, real_y+h), (0, 255, 0), 2)
+    cv2.putText(frame, frame_word, (real_x, real_y - 8), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+            
+# 신뢰도 낮은 단어에 대해 한번 더 체크
+def is_correct(frame, word, roi_x_start, roi_y_start, inflate):
+    Rx, Ry = (int)(x + roi_x_start), (int)(y + roi_y_start)
+    subROI = set_Roi(frame, Rx-inflate, Ry-inflate, (int)(w+inflate*2), (int)(h+inflate*2))
+    
+    pre_subimg = preprocess_image(subROI)
+    result_img = cv2.bilateralFilter(pre_subimg, -1, 10, 5)
+    
+    sub_result = ocr.ocr(result_img, cls=True)
+    cv2.imshow('correct', result_img)
+    
+    # 단어 인식이 안됐거나 기존 단어와 다르게 인식된 경우
+    if (sub_result[0] is None) or (sub_result[0][0][1][0] != word):
+        return False
+        
+    return True
     
 # PaddleOCR 호출 및 옵션 설정(cuda 가속 여부, 언어, inference모델)
 ocr = PaddleOCR(
@@ -56,7 +120,7 @@ ocr = PaddleOCR(
 floorWords = defaultdict(int)
 locationWords = defaultdict(int)
 
-# 이미지 크기(영상 비율 유지)
+# 이미지 크기(영상 비율 유지바람)
 width = 800
 height = 450
 
@@ -71,7 +135,7 @@ most_detfloorWords = "None"
 most_detlocationWords = "None"
 
 # 컴퓨터 카메라, 영상을 사용할 경우
-file_path = '../video_sample/lotte_outlet.avi'
+file_path = '../video_sample/home_plus.avi'
 cap = cv2.VideoCapture(file_path)
 if not cap.isOpened():
     exit
@@ -87,7 +151,7 @@ try:
         frame = cv2.resize(img, (width, height), cv2.INTER_CUBIC) # 해상도 조절
             
         # ROI(관심영역) 설정
-        ROI = frame[roi_y_start: roi_y_start+roi_height, roi_x_start: roi_x_start+roi_width]
+        ROI = set_Roi(frame, roi_x_start, roi_y_start, roi_width, roi_height)
         
         # 전처리
         pre_img = preprocess_image(ROI)
@@ -98,57 +162,41 @@ try:
             
         # 텍스트 인식한 게 있다면
         if result[0] is not None:
-                
+            
             area = defaultdict(int) # 넓이 저장
-                
+            
             # 프레임 한장에 나오는 단어 개수만큼
-            for line in result[0]:
-                points = np.array([line[0][0], line[0][1], line[0][2], line[0][3]], dtype=np.int0)
-                word, confidence = line[1]
-                    
-                # 인식단어 길이가 2 이상 4 이하인 단어만 통과
-                if len(word) < 2 or len(word) > 4:
-                    continue
-                    
-                # 텍스트 위치에서 가장 작은 사각형 찾기
-                x, y, w, h = cv2.boundingRect(points)
-                    
-                # 텍스트 넓이 저장
-                area[word] = w * h
-                    
-            # 해당 장면에서 넓이가 제일 넓은 단어만 추출
-            if area:
-                frame_word = most_area_word(area)
-            else: # 예외 : 길이가 짧은 단어만 검출됐을 경우
+            try:
+                word = search(frame, result[0], area, roi_x_start, roi_y_start)
+            except Exception as e:
+                print(f"Exception: {str(e)}")
+                
+            if word is None:
                 continue
-                    
+            
             # 단어(key)에 대한 카운트(value)
-            if frame_word[0] == 'B' or frame_word[len(frame_word) - 1] == 'F':
-                floorWords[frame_word] += 1 # 층수
+            if word[0] == 'B' or word[len(word) - 1] == 'F':
+                floorWords[word] += 1 # 층수
             else:
-                locationWords[frame_word] += 1 # 구역
+                locationWords[word] += 1 # 구역
                         
             # 텍스트 box만들기
-            str = frame_word #+ " {:.2f}".format(confidence)
-            real_x, real_y = (int)(x + roi_x_start), (int)(y + roi_y_start)
-            cv2.rectangle(frame, (real_x, real_y), (real_x+w, real_y+h), (0, 255, 0), 2)
-            cv2.putText(frame, str, (real_x, real_y - 8), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-  
+            draw_text_box(frame, word, roi_x_start, roi_y_start)
+            
         cv2.imshow('video', frame)
             
         # waitKey() = 프레임 조절
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
             
-    
     # 텍스트 send - 텍스트가 검출되지 않았을 경우 None으로 대체
     if floorWords:
         most_detfloorWords = detect_word(floorWords)
-        print("floor: {}, count: {}".format(most_detfloorWords, floorWords[most_detfloorWords]))
+        print(f"floor: {most_detfloorWords}, count: {floorWords[most_detfloorWords]}")
         
     if locationWords:
         most_detlocationWords = detect_word(locationWords)
-        print("location: {}, count: {}".format(most_detlocationWords, locationWords[most_detlocationWords]))
+        print(f"location: {most_detlocationWords}, count: {locationWords[most_detlocationWords]}")
     
  
 # 오류 catch 
